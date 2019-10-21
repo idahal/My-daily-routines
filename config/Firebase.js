9c4ffd0b1568edd9b7ece9995f8dea9bf96b5c96
Ida Hallner <bee_ida@hotmail.com>
2019-10-21 12:01:46 +0200

    Import firebase in project

---
+++ config/Firebase.js
@@ -0,0 +1,26 @@
+import * as fb from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  MESSAGE_SENDER_ID,
+  APP_ID
+} from "react-native-dotenv";
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: "",
+  messagingSenderId: MESSAGE_SENDER_ID,
+  appId: APP_ID
+};
+
+// Initialize Firebase
+let firebase = fb.initializeApp(firebaseConfig);
+
+export default firebase;
