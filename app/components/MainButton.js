2ca214bdeedf9e0200afad987f738034f1a91128
Ida Hallner <bee_ida@hotmail.com>
2019-10-28 19:02:36 +0100

    Added new component mainbutton

---
+++ app/components/MainButton.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import colors from "../constants/Colors";
+import font from "../constants/Fonts";
+
+class LogoutButton extends Component {
+  render() {
+    const { text, onPress } = this.props;
+    return (
+      <TouchableOpacity style={styles.button} onPress={() => onPress()}>
+        <Text style={styles.buttonText}>{text}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 32,
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    backgroundColor: colors.button,
+    borderRadius: 0,
+    width: 135,
+    height: 50
+  },
+  buttonText: {
+    textTransform: "uppercase",
+    fontFamily: font.extrabold,
+    color: colors.lightWhite,
+    fontSize: 16
+  }
+});
+
+export default LogoutButton;
