b9c484541f7fb93fbd9acfa57639c94a44a577bb
Ida Hallner <bee_ida@hotmail.com>
2019-11-13 19:52:36 +0100

    Added styling to displayroutine

--- src/screens/DisplayRoutineScreen.js
+++ src/screens/DisplayRoutineScreen.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState, useRef } from "react";
-// import { useAuth } from "../config/auth";
+import { useAuth } from "../config/auth";
 import app from "../config/firebase";
 
 import { Button, StyleSheet, Text, View } from "react-native";
 import Timer from "../components/Timer";
+import Title from "../components/Title";
+import HomeButton from "../components/HomeButton";
+import colors from "../constants/Colors";
+import font from "../constants/Fonts";
 
 const DisplayRoutineScreen = props => {
   const { navigation } = props;
@@ -12,11 +16,13 @@ const DisplayRoutineScreen = props => {
   const [activity, setActivity] = useState([]);
   const [total, setTotal] = useState([]);
 
+  // Get user if logged in
+  const { authUser } = useAuth();
+
   //Get params from addRoutineScreen
   const docName = navigation.getParam("name");
   //   const userId = navigation.getParam("addedByUserUid");
   const collectionId = navigation.getParam("keyId");
-  console.log({ docName });
 
   const countTotal = () => {
     if (activity.length > 0) {
@@ -25,12 +31,8 @@ const DisplayRoutineScreen = props => {
         timeArray += item.description;
       });
       setTotal(timeArray);
-
-      console.log(timeArray);
     }
   };
-
-  console.log(total);
 
   useEffect(() => {
     const tempArray = [];
@@ -43,7 +45,6 @@ const DisplayRoutineScreen = props => {
           tempArray.push({ id: doc.id, ...doc.data() });
         });
         setActivity(tempArray);
-        console.log(tempArray);
       });
   }, [db]);
 
@@ -52,22 +53,110 @@ const DisplayRoutineScreen = props => {
   }, [activity]);
 
   return (
-    <View>
-      <Button
-        title="Hem"
+    <View style={styles.container}>
+      <HomeButton
+        text="Hem"
         onPress={() => navigation.navigate("HomeScreen")}
-      ></Button>
-      <Text>{docName}</Text>
-
-      {activity.map(item => (
-        <View key={item.id}>
-          <Text>{item.name}</Text>
-          <Text>{item.description}</Text>
+      ></HomeButton>
+      <Title title={"Mina sparade rutiner"} />
+      {authUser ? (
+        <View style={styles.oneRoutine}>
+          <Text style={styles.routineName}>{docName}</Text>
+          <View style={styles.activityInfo}>
+            <Text style={styles.activityInfoText}>Uppgift</Text>
+            <Text style={styles.activityInfoText}>Tid</Text>
+          </View>
+          {activity.map(item => (
+            <View style={styles.activityCard} key={item.id}>
+              <Text style={styles.activityCardText}>{item.name}</Text>
+              <Text style={styles.activityCardText}>
+                {item.description} min
+              </Text>
+            </View>
+          ))}
+          <Text style={styles.totalText}>
+            Rutinen tar {total} minuter att genomföra.
+          </Text>
+          <Timer total={total} />
         </View>
-      ))}
-      <Timer total={total} />
+      ) : (
+        <Text>Du är inte inloggad</Text>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center"
+  },
+  oneRoutine: {
+    width: "343px",
+    // height: "516px",
+    borderColor: colors.dark,
+    borderWidth: "1px",
+    borderStyle: "solid",
+    marginTop: "1rem",
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+    shadowColor: colors.dark,
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+    marginBottom: "1rem"
+  },
+  routineName: {
+    fontSize: "1.2rem",
+    letterSpacing: "0.05em",
+    fontFamily: font.main,
+    fontWeight: "800",
+    marginTop: "1rem",
+    marginBottom: "2rem",
+    textTransform: "capitalize"
+  },
+  activityInfo: {
+    width: "313px",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: "1rem"
+  },
+  activityInfoText: {
+    fontSize: "1.2rem",
+    letterSpacing: "0.05em",
+    fontFamily: font.main,
+    fontWeight: "800"
+  },
+  activityCard: {
+    width: "313px",
+    height: "50px",
+    borderColor: colors.dark,
+    borderWidth: "1px",
+    borderStyle: "solid",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+    alignItems: "center"
+  },
+  activityCardText: {
+    fontFamily: font.main,
+    fontSize: "1rem",
+    letterSpacing: "0.05em",
+    textTransform: "capitalize"
+  },
+  totalText: {
+    fontFamily: font.main,
+    fontSize: "1rem",
+    letterSpacing: "0.05em",
+    textAlign: "center",
+    marginTop: "1rem"
+  }
+});
 
 export default DisplayRoutineScreen;
