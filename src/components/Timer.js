eb92571a134afb8d5e4fcfaa9077be9a98e9febc
Ida Hallner <bee_ida@hotmail.com>
2019-11-13 19:53:22 +0100

    Added buttons to paus and stop

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,25 +1,32 @@
 import React, { useEffect, useState } from "react";
 // import { useAuth } from "../config/auth";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import MainButton from "../components/MainButton";
+import colors from "../constants/Colors";
+import font from "../constants/Fonts";
 
 // const { navigation } = props;
 
 const Timer = props => {
   // Change useState value to dynamic
   let number = props.total;
-  console.log(number);
   const [count, setCounter] = useState(number);
   const [isActive, setIsActive] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
   function toggle() {
     setCounter(number);
-    setIsActive(!isActive);
+    setIsActive(true);
+    setShowForm(true);
   }
 
-  // Change setCounter value to dynamic
   function reset() {
     setCounter(number);
     setIsActive(false);
+  }
+
+  function paus() {
+    setIsActive(!isActive);
   }
 
   useEffect(() => {
@@ -34,28 +41,57 @@ const Timer = props => {
     return () => clearInterval(interval);
   }, [isActive, count]);
 
-  console.log(count);
   return (
     <View>
-      <Text>{count}</Text>
-
-      <Button title={isActive ? "Paus" : "Start"} onPress={toggle} />
-      <Button title="Stop" onPress={reset} />
+      <MainButton text={"Starta timer"} onPress={toggle} />
+      <Text style={styles.timer}>{count}</Text>
+      {showForm && (
+        <View style={styles.timerButtons}>
+          <TouchableOpacity onPress={paus}>
+            {isActive ? (
+              <Text style={styles.timerText}>Pausa</Text>
+            ) : (
+              <Text style={styles.timerText}>Starta</Text>
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity onPress={reset}>
+            <Text style={styles.timerText}>Stoppa</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
 
 export default Timer;
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "flex-start",
-//     alignItems: "center",
-//     backgroundColor: colors.lightWhite
-//   },
-//   button: {
-//     marginTop: 20,
-//     color: colors.dark
-//   }
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: colors.lightWhite
+  },
+  button: {
+    marginTop: 20,
+    color: colors.dark
+  },
+  timer: {
+    fontFamily: font.main,
+    fontSize: "4rem",
+    textAlign: "center",
+    marginTop: "2rem"
+  },
+  timerButtons: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: "1rem"
+  },
+  timerText: {
+    fontFamily: font.main,
+    fontSize: "1rem",
+    textAlign: "center",
+    marginTop: "1rem",
+    textTransform: "uppercase"
+  }
+});
