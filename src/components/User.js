ba6b1f94d69255e2c7f07bb66445f403f7e0400b
Ida Hallner <bee_ida@hotmail.com>
2019-11-15 10:37:41 +0100

    Deleted unused files

--- src/components/User.js
+++ src/components/User.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { StyleSheet, Text, Button, View } from "react-native";
+// import React from "react";
+// import { StyleSheet, Text, Button, View } from "react-native";
 
-const User = ({ email, children }) => {
-  return (
-    <View style={styles.container}>
-      <Text>{email}</Text>
-      <Text>{children}</Text>
-    </View>
-  );
-};
+// const User = ({ email, children }) => {
+//   return (
+//     <View style={styles.container}>
+//       <Text>{email}</Text>
+//       <Text>{children}</Text>
+//     </View>
+//   );
+// };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: "pink"
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "flex-start",
+//     alignItems: "center",
+//     backgroundColor: "pink"
+//   }
+// });
 
-export default User;
+// export default User;
