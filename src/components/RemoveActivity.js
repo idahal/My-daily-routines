550151c85c33dce055ae3a376aa4f520e74181c6
Ida Hallner <bee_ida@hotmail.com>
2019-11-15 10:24:08 +0100

    Removed unnecessery code

--- src/components/RemoveActivity.js
+++ src/components/RemoveActivity.js
@@ -1,22 +1,13 @@
 import React, { useState } from "react";
 import app from "../config/firebase";
-import { useAuth } from "../config/auth";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 import Xmark from "../icons/Xmark";
-
-// import colors from "../constants/Colors";
-// import font from "../constants/Fonts";
-// import MainButton from "./MainButton";
 
 const RemoveActivity = props => {
   const db = app.firestore();
-
   const itemId = props.id;
   const collectionId = props.collectionId;
-  console.log(itemId);
-  // Get users auth
-  const { authUser } = useAuth();
-
+  
   const activityRef = db
     .collection("routines")
     .doc(collectionId)
@@ -25,7 +16,7 @@ const RemoveActivity = props => {
   const remove = () => activityRef.delete();
 
   return (
-    <View style={styles.container}>
+    <View>
       <TouchableOpacity onPress={remove}>
         <Xmark></Xmark>
       </TouchableOpacity>
@@ -33,11 +24,5 @@ const RemoveActivity = props => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    // justifyContent: "flex-end",
-    // width: "100%"
-  }
-});
 
 export default RemoveActivity;
